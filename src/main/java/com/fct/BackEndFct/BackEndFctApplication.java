d167f7c0408d7a6f3d42ca42066154c9f804ab34
Alby Delgado Morales (ext) <alby.delgado.external@eviden.com>
2024-04-17 09:52:38 +0100

    first commit

---
+++ src/main/java/com/fct/BackEndFct/BackEndFctApplication.java
@@ -0,0 +1,13 @@
+package com.fct.BackEndFct;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackEndFctApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackEndFctApplication.class, args);
+	}
+
+}
